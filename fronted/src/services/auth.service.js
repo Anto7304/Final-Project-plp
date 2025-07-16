86267a1d818524dff055173f666fd11e639710f9
Anto7304 <antonywanyaga2004@gmail.com>
2025-07-16 11:51:43 +0300

    new changes

--- fronted/src/services/auth.service.js
+++ fronted/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL;
+const API = import.meta.env.VITE_API_URL|| "http://localhost:5000/api"
 
 export const register = async (data) => {
   return axios.post(`${API}/signup`, data);
--- fronted/src/services/post.service.js
+++ fronted/src/services/post.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL;// Use env var or fallback
+const API = import.meta.env.VITE_API_URL|| "http://localhost:5000/api";// Use env var or fallback
 
 export const getPosts = async (token) => {
   return axios.get(`${API_URL}/posts`, { headers: { Authorization: `Bearer ${token}` } });
